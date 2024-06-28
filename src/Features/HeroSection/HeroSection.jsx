bd621e4c442972a6faa439980caae3e0c1fc6b72
Etim Precious <123295022+petim7277@users.noreply.github.com>
2024-06-28 13:53:42 +0100

    Started working on Hero Section

--- src/Features/HeroSection/HeroSection.jsx
+++ src/Features/HeroSection/HeroSection.jsx
@@ -1,8 +1,12 @@
-import heroImg from "../../Assets/emojis.jpg"
+import heroImg from "../../Assets/satisfied-lovely-woman-holds-modern-cell-phone 1 1.png"
 const HeroSection = ()=>{
     return (
-        <div className="bg-black  w-full  h-96 mt-8 border-orange-600 border-2 ">
-         <img className="float-right mr-12 w-6/12 h-80 mt-4" src={heroImg} alt={"hero image"}/>
+        <div className="  w-auto  h-auto flex justify-center items-center gap-2.5 ">
+            <div className="pt-4 ">
+                <p className="text-black text-5xl font-bold">Experience Events Like<br/> Never Before</p>
+                <p className=" font-semibold">Discover, Book, and Enjoy the Best Events and Concerts in Your City</p>
+            </div>
+         <img className="  pt-48 h-fit  " src={heroImg} alt={"hero image"}/>
         </div>
     );
 }
