bec2a8a7f7c113e5b2eaf0026135d8bfccc7fec5
Etim Precious <123295022+petim7277@users.noreply.github.com>
2024-06-30 15:14:37 +0100

    working on Navigation Responsiveness

--- src/Features/HeroSection/HeroSection.jsx
+++ src/Features/HeroSection/HeroSection.jsx
@@ -1,12 +1,39 @@
 import heroImg from "../../Assets/satisfied-lovely-woman-holds-modern-cell-phone 1 1.png"
+import heroImgTwo from "../../Assets/Image.png"
 const HeroSection = ()=>{
     return (
-        <div className="  w-auto  h-auto flex justify-center items-center gap-2.5 ">
-            <div className="pt-4 ">
-                <p className="text-black text-5xl font-bold">Experience Events Like<br/> Never Before</p>
-                <p className=" font-semibold">Discover, Book, and Enjoy the Best Events and Concerts in Your City</p>
-            </div>
-         <img className="  pt-48 h-fit  " src={heroImg} alt={"hero image"}/>
+        <div className="flex-col">
+                <div className=" w-auto  h-auto flex justify-center items-center gap-2.5 ">
+                    <div className="pt-4 ">
+                        <p className="text-black text-5xl font-bold">Experience Events Like<br/> Never Before</p>
+                        <p className=" font-semibold">Discover, Book, and Enjoy the Best Events and Concerts in Your City</p>
+                        <p className="font-semibold text-2xl pt-8">Book tickets for ...</p>
+                        <div className="flex gap-10">
+                                <div className="flex-col  pt-8  w-40 justify-center items-center text-center ">
+                                    <p className="border-2 border-orange-600 font-semibold rounded-md">Concerts</p>
+                                    <p className="border-2 border-orange-600 mt-4 font-semibold rounded-md">Conference</p>
+                                    <p className="border-2 border-orange-600 mt-4 font-semibold rounded-md">Theatre</p>
+                                </div>
+                                <div className="flex-col  pt-8 gap-8 w-40 justify-center items-center text-center">
+                                    <p className="border-2 border-orange-600 font-semibold rounded-md">Beach Resort</p>
+                                    <p className="border-2 border-orange-600 mt-4 font-semibold rounded-md">Festivals</p>
+                                    <p className="border-2 border-orange-600 mt-4 font-semibold rounded-md">Family Events</p>
+                                </div>
+                        </div>
+                    </div>
+                 <img className="  pt-48 h-fit" src={heroImg} alt={"hero image"}/>
+                </div>
+
+                <div className=" w-auto  h-auto flex justify-center items-center gap-14 mt-28">
+                    <div>
+                        <img className=" h-fit justify-center items-center" src={heroImgTwo} alt={"second hero image"}/>
+                    </div>
+                    <div>
+                    <p className="text-black text-5xl font-bold  ">Unlock the Best <br/>Events in Your City</p>
+                    <p className="font-semibold pt-4">Simple, Fast, and Reliable Event Ticket Reservations</p>
+                    </div>
+                </div>
+
         </div>
     );
 }
--- src/components/Resuseables/Navbar/NavBar.jsx
+++ src/components/Resuseables/Navbar/NavBar.jsx
@@ -1,28 +1,60 @@
 import { PiCalendarFill } from "react-icons/pi";
-import React from "react";
+import React, {useState} from "react";
 import {Link} from "react-router-dom";
 
 const NavBar = () => {
+   const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
 
   return (
-      <div className="flex  fixed pt-16
-                      w-full gap-12 h-20
-                      justify-center items-center">
+          <nav className="flex  fixed pt-16 w-full gap-12 h-32   bg-white justify-center items-center ">
 
-          <div className="float-left flex ">
-              <PiCalendarFill className="text-4xl text-orange-600"/>
-              <p className="text-black text-2xl" >EventEase</p>
-          </div>
-          <Link to='/' className= "cursor-pointer  hover:text-orange-600  hover:border-2  text-black  rounded-md  border-black hover:w-24  hover:text-center hover:h-10 hover:pt-1">  Home</Link>
-          <Link to='event' className= "cursor-pointer  hover:text-orange-600  hover:border-2  text-black  rounded-md  border-black  hover:w-24  hover:text-center hover:h-10 hover:pt-1">  Events</Link>
-          <Link to='/myTicket' className= "cursor-pointer  hover:text-orange-600  hover:border-2  text-black  rounded-md  border-black  hover:w-24  hover:text-center hover:h-10 hover:pt-1">  My Tickets</Link>
-          <Link to='/categories' className= "cursor-pointer  hover:text-orange-600  hover:border-2  text-black  rounded-md  border-black  hover:w-24  hover:text-center hover:h-10 hover:pt-1">  Categories</Link>
-          <Link to='/search' className= "cursor-pointer  hover:text-orange-600  hover:border-2  text-black  rounded-md  border-black  hover:w-24  hover:text-center hover:h-10 hover:pt-1">  Search</Link>
-          <Link to='/about'  className= "cursor-pointer  hover:text-orange-600  hover:border-2  text-black  rounded-md  border-black  hover:w-24  hover:text-center hover:h-10 hover:pt-1">  About us</Link>
-          <Link to='contactUs' className= "cursor-pointer  hover:text-orange-600  hover:border-2  text-black  rounded-md  border-black  hover:w-24  hover:text-center hover:h-10 hover:pt-1">  Contact us</Link>
-          <button className= "cursor-pointer text-base w-24 h-9 border-2  text-black
-          rounded-md border-orange-600 hover:w-28">Login</button>
-      </div>
+                  <div className="float-left flex ">
+                      <PiCalendarFill className="text-4xl text-orange-600"/>
+                      <p className="text-black text-2xl" >EventEase</p>
+                  </div>
+
+                  <Link to='/' className= "cursor-pointer  hover:text-orange-600  hover:border-2  text-black  rounded-md  border-black hover:w-24  hover:text-center hover:h-10 hover:pt-1">  Home</Link>
+                  <Link to='event' className= "cursor-pointer  hover:text-orange-600  hover:border-2  text-black  rounded-md  border-black  hover:w-24  hover:text-center hover:h-10 hover:pt-1">  Events</Link>
+                  <Link to='/myTicket' className= "cursor-pointer  hover:text-orange-600  hover:border-2  text-black  rounded-md  border-black  hover:w-24  hover:text-center hover:h-10 hover:pt-1">  My Tickets</Link>
+                  <Link to='/categories' className= "cursor-pointer  hover:text-orange-600  hover:border-2  text-black  rounded-md  border-black  hover:w-24  hover:text-center hover:h-10 hover:pt-1">  Categories</Link>
+                  <Link to='/search' className= "cursor-pointer  hover:text-orange-600  hover:border-2  text-black  rounded-md  border-black  hover:w-24  hover:text-center hover:h-10 hover:pt-1">  Search</Link>
+                  <Link to='/about'  className= "cursor-pointer  hover:text-orange-600  hover:border-2  text-black  rounded-md  border-black  hover:w-24  hover:text-center hover:h-10 hover:pt-1">  About us</Link>
+                  <Link to='contactUs' className= "cursor-pointer  hover:text-orange-600  hover:border-2  text-black  rounded-md  border-black  hover:w-24  hover:text-center hover:h-10 hover:pt-1">  Contact us</Link>
+                  <button className= "cursor-pointer text-base w-24 h-9 border-2  text-black
+                  rounded-md border-orange-600 hover:w-28">Login</button>
+                  <button className= "cursor-pointer text-base w-24 h-9 border-2  text-black
+                  rounded-md border-orange-600 hover:w-28">SignUp</button>
+
+                   <div className="md:hidden flex items-center">
+                        <button onClick={toggleMenu} className="focus:outline-none">
+                            <svg className="w-6 h-6 text-black" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d={isOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16m-7 6h7"} />
+                            </svg>
+                        </button>
+                    </div>
+
+                 {isOpen && (
+                     <div className="md:hidden bg-white shadow-md">
+                          <Link to='/' className="block text-black hover:text-orange-600 p-2 border-b">Home</Link>
+                           <Link to='/event' className="block text-black hover:text-orange-600 p-2 border-b">Events</Link>
+                           <Link to='/myTicket' className="block text-black hover:text-orange-600 p-2 border-b">My Tickets</Link>
+                           <Link to='/categories' className="block text-black hover:text-orange-600 p-2 border-b">Categories</Link>
+                           <Link to='/search' className="block text-black hover:text-orange-600 p-2 border-b">Search</Link>
+                           <Link to='/about' className="block text-black hover:text-orange-600 p-2 border-b">About us</Link>
+                           <Link to='/contactUs' className="block text-black hover:text-orange-600 p-2 border-b">Contact us</Link>
+                          <button className="block w-full text-base p-2 border-2 text-black rounded-md border-orange-600 hover:w-full">Login</button>
+                     </div>)}
+          </nav>
   );
 }
 export default NavBar;
+
+
+
+
+
+
